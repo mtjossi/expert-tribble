0b97b22d7e28d5e07502e38199aab10f776fc9dd
mtjossi <45386128+mtjossi@users.noreply.github.com>
2022-12-30 11:13:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,68 +1,83 @@
 import streamlit as st
 import requests
+from bs4 import BeautifulSoup
 import pandas as pd
+from urllib3.exceptions import InsecureRequestWarning
+from urllib3 import disable_warnings
 
-URL = "https://www.hsx.vn/Modules/Listed/Web/SymbolList?pageFieldName1=Code&pageFieldValue1=&pageFieldOperator1=eq&pageFieldName2=Sectors&pageFieldValue2=&pageFieldOperator2=&pageFieldName3=Sector&pageFieldValue3=00000000-0000-0000-0000-000000000000&pageFieldOperator3=&pageFieldName4=StartWith&pageFieldValue4=&pageFieldOperator4=&pageCriteriaLength=4&_search=false&nd=1667637899557&rows=1000&page=1&sidx=id&sord=desc"
-r = requests.get(URL).json()
+disable_warnings(InsecureRequestWarning)
 
-all_rows = []
-for i in range(len(r['rows'])):
-    row_data = [r['rows'][i]['cell'][1], r['rows'][i]['cell'][2], r['rows'][i]['cell'][6]]
-    all_rows.append(row_data)
+needed = ['ABI', 'BIC', 'BSI', 'CDN', 'CTD', 'DAD', 'DSN', 'DVP','DXG', 'EID', 'EVE',
+ 'FMC', 'FPT', 'GDT','HC3', 'HNI', 'HPP', 'HUB', 'KDH', 'LBM', 'LPB', 'LTG', 'MBB',
+ 'MCH', 'MIG', 'MPC', 'MWG', 'NSC', 'PHP', 'PTB', 'PVI', 'PVT', 'REE', 'SCS', 'SHB',
+ 'SSI', 'SZL', 'TCT', 'THG', 'TLG', 'TMS', 'TPB', 'TV2', 'VGG', 'VND', 'VNM', 'VTP',
+ 'WSB']
 
-df = pd.DataFrame(columns=['Ticker', 'ISIN', 'Outstanding Volume'], data=all_rows)
+hsx_needed = ['BIC','BSI', 'CTD', 'DSN', 'DVP', 'DXG', 'EVE', 'FMC', 'FPT', 'GDT',
+ 'HUB', 'KDH', 'LBM', 'LPB', 'MBB', 'MIG', 'MWG', 'NSC', 'PTB', 'PVT', 'REE', 'SCS',
+ 'SHB', 'SSI', 'SZL', 'TCT', 'THG', 'TLG', 'TMS', 'TPB', 'TV2', 'VND', 'VNM']
 
-st.header("Differences?")
+hnx_needed = ['CDN', 'DAD', 'EID', 'PVI', 'ABI', 'HC3', 'HNI', 'HPP', 'LTG', 'MCH',
+ 'MPC', 'PHP', 'VGG', 'VTP', 'WSB']
 
-default_list = ['BIC',
- 'BSI',
- 'CTD',
- 'DSN',
- 'DVP',
- 'DXG',
- 'EVE',
- 'FMC',
- 'FPT',
- 'GDT',
- 'HUB',
- 'KDH',
- 'LBM',
- 'LPB',
- 'MBB',
- 'MIG',
- 'MWG',
- 'NSC',
- 'PTB',
- 'PVT',
- 'REE',
- 'SCS',
- 'SHB',
- 'SSI',
- 'SZL',
- 'TCT',
- 'THG',
- 'TLG',
- 'TMS',
- 'TPB',
- 'TV2',
- 'VND',
- 'VNM']
 
-st.sidebar.header("Which tickers do you want to compare?")
-selected_tickers = st.sidebar.multiselect("Enter the Tickers you want to compare:", df['Ticker'].to_list(), default=default_list)
+st.title("Testing3")
+#############################################
+start_butt = st.button("Start:")
+if start_butt:
+    URL1 = "https://www.hsx.vn/Modules/Listed/Web/SymbolList?pageFieldName1=Code&pageFieldValue1=&pageFieldOperator1=eq&pageFieldName2=Sectors&pageFieldValue2=&pageFieldOperator2=&pageFieldName3=Sector&pageFieldValue3=00000000-0000-0000-0000-000000000000&pageFieldOperator3=&pageFieldName4=StartWith&pageFieldValue4=&pageFieldOperator4=&pageCriteriaLength=4&_search=false&nd=1667637899557&rows=1000&page=1&sidx=id&sord=desc"
+    r = requests.get(URL1).json()
 
-final_df = df[df['Ticker'].isin(selected_tickers)]
-st.dataframe(final_df)
+    all_rows = []
+    for i in range(len(r['rows'])):
+        ticker = r['rows'][i]['cell'][1]
+        out_vol = r['rows'][i]['cell'][6].replace('.','').replace(',','.')
+        row_data = [ticker, float(out_vol)]
+        all_rows.append(row_data)
+        
+    df1 = pd.DataFrame(columns=['Ticker', 'Outstanding Volume'], data=all_rows)
+    df1['Outstanding Volume'] = df1['Outstanding Volume']
 
-def convert_df(df):
-    return df.to_csv(index=False).encode('utf-8')
+    df_hsx = df1.copy()
+    df_hsx = df_hsx[df_hsx['Ticker'].isin(hsx_needed)]
+    #################################################################
 
-csv = convert_df(final_df)
+    hnx_needed_dict = {}
+    hnx_ticker_list = []
+    hnx_os_list = []
 
-st.download_button(
-    "Click to Download",
-    csv,
-    'file.csv',
-    "text/csv",
-    key='download-csv'
-)
+    for t in hnx_needed:
+        hnx_url = f"https://hnx.vn/en-gb/cophieu-etfs/chi-tiet-chung-khoan-ny-{t}.html?_des_tab=1"
+        r4 = requests.get(hnx_url, verify=False)
+        
+        soup = BeautifulSoup(r4.text)
+        div_class = "dktimkiem_cell_content"
+        all_data = soup.find_all("div", class_=div_class)
+        all_data_list = []
+        for i in all_data:
+            all_data_list.append(i.get_text().split())
+
+        out_shares = int(all_data_list[-2][0].replace(',', ''))
+        hnx_ticker_list.append(t)
+        hnx_os_list.append(out_shares)
+
+    hnx_needed_dict['Ticker'] = hnx_ticker_list
+    hnx_needed_dict['Outstanding Volume'] = hnx_os_list
+
+    df_hnx = pd.DataFrame.from_dict(hnx_needed_dict)
+    ##########################
+    df_combo = pd.concat([df_hsx, df_hnx])
+    df_combo = df_combo.sort_values(by='Ticker').reset_index(drop=True)
+
+    def convert_df(df):
+        return df.to_csv(index=False).encode('utf-8')
+
+    csv = convert_df(df_combo)
+
+    st.download_button(
+        "Click to Download",
+        csv,
+        'file.csv',
+        "text/csv",
+        key='download-csv'
+    )
